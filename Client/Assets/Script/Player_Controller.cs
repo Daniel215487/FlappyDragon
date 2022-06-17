e2e8f0a4fcf929a97323b6dbae12fe64c5e83f51
black215487 <black215487@gmail.com>
2022-06-17 12:36:27 +0800

    [Add] 添加飛龍素材 [Dev] 飛龍墜落&飛行完成

---
+++ Client/Assets/Script/Player_Controller.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class Player_Controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform PlayerTrf;
+    private int JumpHeight = 4;
+    private float ForcePower = 0.02f;
+    private bool _isFalling = true;
+    private float _offset;
+    private Vector3 _newpos;
+    private Player_State PState;
+    void Start()
+    {
+        PState = new Player_State();
+        if(PlayerTrf!=null)
+            PState.Bind(PlayerTrf);
+        else
+            Debug.LogWarning("沒掛玩家物件");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_isFalling)
+            Fall();
+        else
+            Jump();
+    }
+    public void OnClickToJump(){
+        _newpos = PlayerTrf.position+Vector3.up*JumpHeight;
+        _isFalling=false;
+        PState.SetPlayerState(Player_State.PState.Jump);
+    }
+    private void Fall(){
+        PlayerTrf.position+=Vector3.down*ForcePower*0.8f;
+    }
+    private void Jump(){
+        _offset = Vector3.Magnitude(_newpos-PlayerTrf.position);
+        // print($"offset:{_offset}");
+        if(_offset<0.5f){
+            _isFalling=true;
+            PState.SetPlayerState(Player_State.PState.Fall);
+        }
+        else{
+            PlayerTrf.position+=Vector3.up*ForcePower;
+        }
+    }
+}
---
+++ Client/Assets/Script/Player_State.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Player_State {
+    public Transform PlayerTrf;
+    private PState _pState;
+
+    public void Bind(Transform ptf){
+        PlayerTrf = ptf;
+    }
+    public void SetPlayerState(PState state){
+        _pState=state;
+        ChangeState();
+    }
+    private void ChangeState(){
+        switch(_pState){
+            case PState.None:
+                break;
+            case PState.Fall:
+                PlayerTrf.eulerAngles = Vector3.back*15;
+                break;
+            case PState.Jump:
+                PlayerTrf.eulerAngles = Vector3.forward*25;
+                break;
+        }
+    } 
+
+    public enum PState{
+        None,
+        Fall,
+        Jump
+    }
+}
